987e5f3756cd80ef112514ff66b26ac265ee2196
yangming <shanghaiyangming@gmail.com>
2014-03-03 11:32:00 +0800

    增加图片处理功能

--- module/Service/src/Service/Controller/FileController.php
+++ module/Service/src/Service/Controller/FileController.php
@@ -29,9 +29,14 @@ class FileController extends Action
     public function indexAction()
     {
         $id = $this->params()->fromRoute('id', null);
-        $download = $this->params()->fromRoute('download', null);
-        $width = $this->params()->fromRoute('w', null);
-        $height = $this->params()->fromRoute('h', null);
+        $download = $this->params()->fromRoute('d', false);
+        $resize = $this->params()->fromRoute('r', false);
+        $thumbnail = $this->params()->fromRoute('t', false);
+        $adpter = $this->params()->fromRoute('a', false);
+        $width = intval($this->params()->fromRoute('w', 0));
+        $height = intval($this->params()->fromRoute('h', 0));
+        $quality = intval($this->params()->fromRoute('q', 100));
+        
         if ($id == null) {
             header("HTTP/1.1 404 Not Found");
             return $this->response;
@@ -39,13 +44,54 @@ class FileController extends Action
         
         $gridFsFile = $this->_file->getGridFsFileById($id);
         if ($gridFsFile instanceof \MongoGridFSFile) {
-            if (strpos(strtolower($gridFsFile->file['mime']), 'image')) {
+            if (strpos(strtolower($gridFsFile->file['mime']), 'image') !== false) {
                 // 图片处理
+                $fileInfo = $gridFsFile->file;
+                $fileName = $fileInfo['filename'];
+                $fileMime = $fileInfo['mime'];
+                
                 $imagick = new \Imagick();
                 $resource = $gridFsFile->getResource();
                 $imagick->readImageFile($resource);
-                $imagick->thumbnailimage($width, $height, false, false);
-                echo $imagick->getimageblob();
+                if ($adpter) {
+                    $imagick->cropThumbnailImage($width, $height);
+                } elseif ($thumbnail) {
+                    $imagick->thumbnailImage($width, $height);
+                } else {
+                    $geo = $imagick->getImageGeometry();
+                    $sizeWidth = $geo['width'];
+                    $sizeHeight = $geo['height'];
+                    if ($width > 0 && $height > 0) {
+                        if ($sizeWidth / $width > $sizeHeight / $height) {
+                            $height = 0;
+                        } else {
+                            $width = 0;
+                        }
+                        $imagick->thumbnailImage($width, $height);
+                    } else 
+                        if ($width > 0 || $height > 0) {
+                            $imagick->thumbnailImage($width, $height);
+                        }
+                }
+                if ($quality < 100) {
+                    $imagick->setImageCompression(\Imagick::COMPRESSION_JPEG);
+                    $imagick->setImageCompressionQuality($quality);
+                    $fileName .= '.jpg';
+                    $fileMime = 'image/jpg';
+                }
+                $imagick->stripImage();
+                $data = $imagick->getimageblob();
+                $imagick->destroy();
+                
+                setHeaderExpires();
+                if (isset($fileMime)) {
+                    header('Content-Type: ' . $fileMime . ';');
+                }
+                if ($download)
+                    header('Content-Disposition:attachment;filename="' . $fileName . '"');
+                else
+                    header('Content-Disposition:filename="' . $fileName . '"');
+                echo $data;
             } else {
                 $this->_file->output($gridFsFile, true, $download == null ? false : true);
             }
