2015b85ec09afd8eab43be297e07ddde543defa6
yangming <shanghaiyangming@gmail.com>
2014-03-25 13:58:44 +0800

    更新

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -54,9 +54,6 @@ class Module
             if ($contentType) {
                 $contentType = $contentType->getFieldValue();
                 if (strpos($contentType, 'application/json') !== false) {
-                    // $bc = new Browscap(ROOT_PATH . '/cache/');
-                    // $current_browser = $bc->getBrowser();
-                    // if ($current_browser->Browser === 'IE')
                     $objHeaders->addHeaderLine('Content-Type', 'text/html;charset=utf-8');
                 }
             } else {
