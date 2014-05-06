40be17a37b19f9277dba9c4a7b312cece83d5339
yangming <shanghaiyangming@gmail.com>
2014-05-06 11:07:16 +0800

    更新

--- library/My/Common/Plugin/Collection.php
+++ library/My/Common/Plugin/Collection.php
@@ -24,10 +24,11 @@ class Collection extends AbstractPlugin
 
     /**
      * 初始化集合调用
-     * @param string $collection
-     * @param string $database
-     * @param string $cluster
-     * @param string $collectionOptions
+     * 
+     * @param string $collection            
+     * @param string $database            
+     * @param string $cluster            
+     * @param string $collectionOptions            
      * @throws \Exception
      * @return \My\Common\MongoCollection
      */
@@ -42,28 +43,52 @@ class Collection extends AbstractPlugin
         
         return new MongoCollection($mongoConfig, $collection, $database, $cluster, $collectionOptions);
     }
-    
 
     /**
      * 初始化集合并通过复制集来获取数据
-     * 
-     * @param string $collection
-     * @param string $database
-     * @param string $cluster
-     * @param string $collectionOptions
+     *
+     * @param string $collection            
+     * @param string $database            
+     * @param string $cluster            
+     * @param string $collectionOptions            
      * @throws \Exception
      * @return \My\Common\MongoCollection
      */
-    public function secondary($collection = null, $database = DEFAULT_DATABASE, $cluster = DEFAULT_CLUSTER, $collectionOptions = null) {
+    public function secondary($collection = null, $database = DEFAULT_DATABASE, $cluster = DEFAULT_CLUSTER, $collectionOptions = null)
+    {
         if ($collection === null)
             throw new \Exception('请设定集合名称');
         
         $mongoConfig = $this->getController()
-        ->getServiceLocator()
-        ->get('mongos');
+            ->getServiceLocator()
+            ->get('mongos');
         
         $obj = new MongoCollection($mongoConfig, $collection, $database, $cluster, $collectionOptions);
         $obj->setReadPreference(\MongoClient::RP_SECONDARY);
         return $obj;
     }
+
+    /**
+     * 最快速写入模式,读取从集群，写入不等待返回错误
+     * 
+     * @param string $collection            
+     * @param string $database            
+     * @param string $cluster            
+     * @param string $collectionOptions            
+     * @return \My\Common\MongoCollection
+     */
+    public function qw($collection = null, $database = DEFAULT_DATABASE, $cluster = DEFAULT_CLUSTER, $collectionOptions = null)
+    {
+        $obj = new MongoCollection($mongoConfig, $collection, $database, $cluster, $collectionOptions);
+        $obj->setReadPreference(\MongoClient::RP_SECONDARY);
+        if (method_exists($obj, 'setWriteConcern')) {
+            $obj->setWriteConcern(0);
+        } else {
+            $obj->w = 0;
+        }
+        return $obj;
+    }
 }
+
+
+
