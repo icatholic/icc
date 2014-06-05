eaa56b4ce409761ec46df68b690203c48003ee89
yangming <shanghaiyangming@gmail.com>
2014-06-05 18:31:39 +0800

    更新

--- module/Gearman/src/Gearman/Controller/IndexController.php
+++ module/Gearman/src/Gearman/Controller/IndexController.php
@@ -7,8 +7,12 @@ use My\Common\Controller\Action;
 class IndexController extends Action
 {
 
+    private $_worker;
+
     public function init()
-    {}
+    {
+        $this->_worker = $this->gearman()->worker();
+    }
 
     public function indexAction()
     {
@@ -16,52 +20,32 @@ class IndexController extends Action
         return $this->response;
     }
 
+    /**
+     * 处理map reduce统计
+     * 
+     * @return string
+     */
     public function mrAction()
     {
-        $gmworker = new GearmanWorker();
+        $gmworker->addFunction("mapreduce", array(
+            $this,
+            'mapReduceWorker'
+        ));
         
-        // Add default server (localhost).
-        $gmworker->addServer();
-        
-        // Register function "reverse" with the server. Change the worker function to
-        // "reverse_fn_fast" for a faster worker with no output.
-        $gmworker->addFunction("reverse", "reverse_fn");
-        
-        print "Waiting for job...\n";
-        while ($gmworker->work()) {
-            if ($gmworker->returnCode() != GEARMAN_SUCCESS) {
-                echo "return_code: " . $gmworker->returnCode() . "\n";
-                break;
+        while ($this->_worker->work()) {
+            if ($this->_worker->returnCode() != GEARMAN_SUCCESS) {
+                echo "return_code: " . $this->_worker->returnCode() . "\n";
             }
         }
+    }
 
-        function reverse_fn($job)
-        {
-            echo "Received job: " . $job->handle() . "\n";
-            
-            $workload = $job->workload();
-            $workload_size = $job->workloadSize();
-            
-            echo "Workload:  $workload  ( $workload_size )\n";
-            
-            // This status loop is not needed, just showing how it works
-            for ($x = 0; $x < $workload_size; $x ++) {
-                echo "Sending status: " . ($x + 1) . "/ $workload_size  complete\n";
-                $job->sendStatus($x, $workload_size);
-                sleep(1);
-            }
-            
-            $result = strrev($workload);
-            echo "Result:  $result \n";
-            
-            // Return what we want to send back to the client.
-            return $result;
-        }
-        
-        // A much simpler and less verbose version of the above function would be:
-        function reverse_fn_fast($job)
-        {
-            return strrev($job->workload());
+    private function mapReduceWorker(\GearmanJob $job)
+    {
+        try {
+            $job->handle();
+            $params = unserialize($job->workload());
+        } catch (\Exception $e) {
+            var_dump($e);
         }
     }
 }
