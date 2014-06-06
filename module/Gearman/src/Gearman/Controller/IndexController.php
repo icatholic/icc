84b281daa3db18000e3f99374d5f93ca389e21e2
Young <shanghaiyangming@gmail.com>
2014-06-06 15:29:32 +0800

    调整

--- module/Gearman/src/Gearman/Controller/IndexController.php
+++ module/Gearman/src/Gearman/Controller/IndexController.php
@@ -51,34 +51,39 @@ class IndexController extends Action
         }
     }
 
+    /**
+     * 处理mr的worker方法
+     * 
+     * @param \GearmanJob $job            
+     * @return boolean
+     */
     private function mapReduceWorker(\GearmanJob $job)
     {
         try {
             $job->handle();
-            $params = unserialize($job->workload());
             
+            $params = unserialize($job->workload());
             $out = $params['out'];
             $dataModel = $params['dataModel'];
             $statisticInfo = $params['statisticInfo'];
             $query = $params['query'];
             $method = $params['method'];
-            
             $rst = mapReduce($out, $dataModel, $statisticInfo, $query, $method);
             
             if (is_array($rst) && isset($rst['ok']) && $rst['ok'] === 0) {
                 switch ($rst['code']) {
-                	case 500:
-                	    $job->sendWarning('根据查询条件，未检测到有效的统计数据');
-                	    break;
-                	case 501:
-                	    $job->sendWarning('MapReduce执行失败，原因：' . $rst['msg']);
-                	    break;
-                	case 502:
-                	    $job->sendWarning('程序正在执行中，请勿频繁尝试');
-                	    break;
-                	case 503:
-                	    $job->sendWarning('程序异常：' . $rst['msg']);
-                	    break;
+                    case 500:
+                        $job->sendWarning('根据查询条件，未检测到有效的统计数据');
+                        break;
+                    case 501:
+                        $job->sendWarning('MapReduce执行失败，原因：' . $rst['msg']);
+                        break;
+                    case 502:
+                        $job->sendWarning('程序正在执行中，请勿频繁尝试');
+                        break;
+                    case 503:
+                        $job->sendWarning('程序异常：' . $rst['msg']);
+                        break;
                 }
                 $job->sendFail();
                 return false;
