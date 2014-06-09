18f7b5a77d498f6ecbf0821a613ad6e19dcc860e
yangming <shanghaiyangming@gmail.com>
2014-06-09 21:34:48 +0800

    更新

--- module/Gearman/src/Gearman/Controller/IndexController.php
+++ module/Gearman/src/Gearman/Controller/IndexController.php
@@ -1,15 +1,13 @@
 <?php
 namespace Gearman\Controller;
 
-use Zend\View\Model\ViewModel;
-use Zend\Console\Response as ConsoleResponse;
 use My\Common\Controller\Action;
 
 class IndexController extends Action
 {
 
     private $_worker;
-    
+
     private $_data;
 
     public function init()
@@ -26,7 +24,7 @@ class IndexController extends Action
 
     public function cacheAction()
     {
-        $key = $this->params('key',null);
+        $key = $this->params('key', null);
         if (($rst = $this->cache()->load($key)) === null) {
             $datas = 'my cache content';
             $this->cache()->save($datas);
@@ -44,10 +42,48 @@ class IndexController extends Action
      */
     public function mrAction()
     {
-        $this->_worker->addFunction("mapreduce", array(
-            new static(),
-            'mapReduceWorker'
-        ));
+        $cache = $this->cache();
+        $this->_worker->addFunction("mapreduce", function (\GearmanJob $job) use($cache)
+        {
+            try {
+                $job->handle();
+                $params = unserialize($job->workload());
+                var_dump($params);
+                $out = $params['out'];
+                $this->_data->setCollection($params['dataCollection']);
+                $dataModel = $this->_data;
+                $statisticInfo = $params['statisticInfo'];
+                $query = $params['query'];
+                $method = $params['method'];
+                $rst = mapReduce($out, $dataModel, $statisticInfo, $query, $method);
+                var_dump($rst);
+                $cache->remove($out);
+                if (is_array($rst) && isset($rst['ok']) && $rst['ok'] === 0) {
+                    switch ($rst['code']) {
+                        case 500:
+                            $job->sendWarning('根据查询条件，未检测到有效的统计数据');
+                            break;
+                        case 501:
+                            $job->sendWarning('MapReduce执行失败，原因：' . $rst['msg']);
+                            break;
+                        case 502:
+                            $job->sendWarning('程序正在执行中，请勿频繁尝试');
+                            break;
+                        case 503:
+                            $job->sendWarning('程序异常：' . $rst['msg']);
+                            break;
+                    }
+                    $job->sendFail();
+                    return false;
+                }
+                // sleep(30);//成功的操作等待30秒，用以确保复制集完成同步
+                $job->sendComplete();
+                return true;
+            } catch (\Exception $e) {
+                var_dump(exceptionMsg($e));
+                $job->sendException(exceptionMsg($e));
+            }
+        });
         
         while ($this->_worker->work()) {
             if ($this->_worker->returnCode() != GEARMAN_SUCCESS) {
@@ -58,50 +94,51 @@ class IndexController extends Action
         return $this->response;
     }
 
-    /**
-     * 处理mr的worker方法
-     * 
-     * @param \GearmanJob $job            
-     * @return boolean
-     */
-    public function mapReduceWorker(\GearmanJob $job)
-    {
-        try {
-            $job->handle();
-            $params = unserialize($job->workload());
-            var_dump($params);
-            $out = $params['out'];
-            $dataModel = $this->collection()->secondary($params['dataCollection']);
-            $statisticInfo = $params['statisticInfo'];
-            $query = $params['query'];
-            $method = $params['method'];
-            $rst = mapReduce($out, $dataModel, $statisticInfo, $query, $method);
-            var_dump($rst);
-            $this->cache()->remove($out);
-            if (is_array($rst) && isset($rst['ok']) && $rst['ok'] === 0) {
-                switch ($rst['code']) {
-                    case 500:
-                        $job->sendWarning('根据查询条件，未检测到有效的统计数据');
-                        break;
-                    case 501:
-                        $job->sendWarning('MapReduce执行失败，原因：' . $rst['msg']);
-                        break;
-                    case 502:
-                        $job->sendWarning('程序正在执行中，请勿频繁尝试');
-                        break;
-                    case 503:
-                        $job->sendWarning('程序异常：' . $rst['msg']);
-                        break;
-                }
-                $job->sendFail();
-                return false;
-            }
-            //sleep(30);//成功的操作等待30秒，用以确保复制集完成同步
-            $job->sendComplete();
-            return true;
-        } catch (\Exception $e) {
-            var_dump(exceptionMsg($e));
-            $job->sendException(exceptionMsg($e));
-        }
-    }
+/**
+ * 处理mr的worker方法
+ *
+ * @param \GearmanJob $job            
+ * @return boolean
+ */
+    // public function mapReduceWorker(\GearmanJob $job)
+    // {
+    // try {
+    // $job->handle();
+    // $params = unserialize($job->workload());
+    // var_dump($params);
+    // $out = $params['out'];
+    // $this->_data->setCollection($params['dataCollection']);
+    // $dataModel = $this->_data;
+    // $statisticInfo = $params['statisticInfo'];
+    // $query = $params['query'];
+    // $method = $params['method'];
+    // $rst = mapReduce($out, $dataModel, $statisticInfo, $query, $method);
+    // var_dump($rst);
+    // $this->cache()->remove($out);
+    // if (is_array($rst) && isset($rst['ok']) && $rst['ok'] === 0) {
+    // switch ($rst['code']) {
+    // case 500:
+    // $job->sendWarning('根据查询条件，未检测到有效的统计数据');
+    // break;
+    // case 501:
+    // $job->sendWarning('MapReduce执行失败，原因：' . $rst['msg']);
+    // break;
+    // case 502:
+    // $job->sendWarning('程序正在执行中，请勿频繁尝试');
+    // break;
+    // case 503:
+    // $job->sendWarning('程序异常：' . $rst['msg']);
+    // break;
+    // }
+    // $job->sendFail();
+    // return false;
+    // }
+    // // sleep(30);//成功的操作等待30秒，用以确保复制集完成同步
+    // $job->sendComplete();
+    // return true;
+    // } catch (\Exception $e) {
+    // var_dump(exceptionMsg($e));
+    // $job->sendException(exceptionMsg($e));
+    // }
+    // }
 }
