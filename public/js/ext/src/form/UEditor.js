b2196d9ab4d4ec4974d65e200fb953a24a84718a
Young <shanghaiyangming@gmail.com>
2014-06-19 23:47:50 +0800

    调整ueditor整合功能，明天添加服务器端的上传文件受理

--- public/js/ext/src/form/UEditor.js
+++ public/js/ext/src/form/UEditor.js
@@ -1,5 +1,5 @@
-Ext.define('Ext.form.UEditor',{
-    extend:Ext.form.FieldContainer,
+Ext.define('Ext.form.UEditor', {
+    extend: Ext.form.FieldContainer,
     mixins: {
         field: Ext.form.field.Field
     },
@@ -7,28 +7,31 @@ Ext.define('Ext.form.UEditor',{
     alternateClassName: 'Ext.ux.UEditor',
     ueditorInstance: null,
     initialized: false,
-    initComponent: function () {
+    initComponent: function() {
         var me = this;
         me.addEvents('initialize', 'change');
         var id = me.id + '-ueditor';
         me.html = '<script id="' + id + '" type="text/plain" name="' + me.name + '"></script>';
         me.callParent(arguments);
         me.initField();
-        me.on('render', function () {
+        me.on('render', function() {
             var width = me.width - 105;
             var height = me.height - 109;
-            var config = {initialFrameWidth: width, initialFrameHeight: height};
+            var config = {
+                initialFrameWidth: width,
+                initialFrameHeight: height
+            };
             me.ueditorInstance = UE.getEditor(id, config);
-            me.ueditorInstance.ready(function () {
+            me.ueditorInstance.ready(function() {
                 me.initialized = true;
                 me.fireEvent('initialize', me);
-                me.ueditorInstance.addListener('contentChange', function () {
+                me.ueditorInstance.addListener('contentChange', function() {
                     me.fireEvent('change', me);
                 });
             });
         });
     },
-    getValue: function () {
+    getValue: function() {
         var me = this,
             value = '';
         if (me.initialized) {
@@ -37,17 +40,21 @@ Ext.define('Ext.form.UEditor',{
         me.value = value;
         return value;
     },
-    setValue: function (value) {
+    setValue: function(value) {
         var me = this;
         if (value === null || value === undefined) {
             value = '';
         }
         if (me.initialized) {
             me.ueditorInstance.setContent(value);
+        } else {
+            me.on('initialize', function() {
+                me.ueditorInstance.setContent(value);
+            });
         }
         return me;
     },
-    onDestroy: function () {
+    onDestroy: function() {
         this.ueditorInstance.destroy();
     }
 });
